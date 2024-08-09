a57ade8d49877b7d6ece26d514ec75c66d4f22b5
vtAndreola <dev.andreola@gmail.com>
2024-08-09 18:43:19 -0300

    feat: add social media

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,42 @@
 import { Textarea } from "@/components/ui/textarea";
 import { VoiceList } from "../components/voice-list";
+import { BsFileWordFill, BsGithub, BsLinkedin } from "react-icons/bs";
+import { CgWebsite } from "react-icons/cg";
+import { BiWorld } from "react-icons/bi";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
   return (
-    <div className="flex flex-col items-center max-h-full">
-      <div className="py-2 text-center border px-4 bg-white rounded-md my-2">
+    <div className="flex relative flex-col items-center max-h-full">
+      <div className="py-2 flex-shrink-0 text-center border px-4 bg-white rounded-md my-2">
         <h1 className="font-bold text-xl">Text to Speech</h1>
         <p className="text-sm text-muted-foreground">
           Converta textos para voz
         </p>
+      </div>
+
+      <div className="absolute right-3 top-3">
+        <div className="flex items-center justify-center ">
+          <Link target="_blank" href={"https://github.com/dev-andreola"}>
+            <Button variant={"ghost"} size={"icon"}>
+              <BsGithub size={22} />
+            </Button>
+          </Link>
+          <Link
+            target="_blank"
+            href={"https://www.linkedin.com/in/dev-andreola/"}
+          >
+            <Button variant={"ghost"} size={"icon"}>
+              <BsLinkedin size={22} />
+            </Button>
+          </Link>
+          <Link target="_blank" href={"https://vt-code.vercel.app/"}>
+            <Button variant={"ghost"} size={"icon"}>
+              <BiWorld size={22} />
+            </Button>
+          </Link>
+        </div>
       </div>
 
       <VoiceList />
