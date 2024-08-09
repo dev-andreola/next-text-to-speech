4dd17e002c6ea6fc993b1d54119a532245104c67
vtAndreola <dev.andreola@gmail.com>
2024-08-09 16:11:23 -0300

    feat: populate voice data

--- components/voice-list.tsx
+++ components/voice-list.tsx
@@ -11,126 +11,64 @@ import {
   CardTitle,
 } from "./ui/card";
 import { Textarea } from "./ui/textarea";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Voice } from "elevenlabs/api";
 
 export default function VoiceList() {
+  const [voices, setVoices] = useState<Voice[]>([]);
+
+  useEffect(() => {
+    async function fetchVoices() {
+      try {
+        const response = await fetch("/api/get-voices");
+        const data = await response.json();
+        setVoices(data.voices);
+      } catch (error) {
+        console.error("Falha ao buscar vozes:", error);
+      }
+    }
+
+    fetchVoices();
+  }, []);
+
   return (
     <div className="w-full px-2">
       <Textarea placeholder="Digite aqui o seu texto..." />
       <div className="py-2">
         <h2 className="font-semibold">Escolha uma das vozes:</h2>
-        <Card className=" py-2 px-4 space-y-2">
-          <CardHeader className="p-0 flex flex-col items-center">
-            <CardTitle>Elias</CardTitle>
-            <CardDescription className="">
-              <div>model</div>
-            </CardDescription>
-          </CardHeader>
-          <div className="text-xs flex-1 text-muted-foreground flex justify-between px-2">
-            <div className="flex flex-col items-center justify-center">
-              <span className="font-semibold">sila</span>
-              <span>doaiwdoaw</span>
+        {voices.map((voice) => (
+          <Card key={voice.voice_id} className=" py-2 px-4 space-y-3">
+            <CardHeader className="p-0 flex flex-col justify-start items-center">
+              <CardTitle>{voice.name}</CardTitle>
+              <CardDescription className="leading-tight	">
+                {voice.category}
+              </CardDescription>
+            </CardHeader>
+            <div className="text-xs flex-1 text-muted-foreground grid grid-cols-3 gap-2 px-2">
+              {voice.labels &&
+                Object.entries(voice.labels).map(([key, value]) => (
+                  <div
+                    key={voice.voice_id}
+                    className="flex border roudned-sm py-1 flex-col items-center justify-center"
+                  >
+                    <span className="font-semibold">{key}</span>
+                    <span>{value}</span>
+                  </div>
+                ))}
             </div>
-            <div className="flex flex-col items-center">
-              <span className="font-semibold">sila</span>
-              <span>doaiwdoaw</span>
-            </div>
-            <div className="flex flex-col items-center">
-              <span className="font-semibold">sila</span>
-              <span>doaiwdoaw</span>
-            </div>
-            <div className="flex flex-col items-center">
-              <span className="font-semibold">sila</span>
-              <span>doaiwdoaw</span>
-            </div>
-          </div>
 
-          <CardContent className="py-2 px-0 space-y-2">
-            <Button variant={"outline"} className="w-full gap-2">
-              <span>Preview</span>
-              <FaPlayCircle size={22} />
-            </Button>
-            <Button className="w-full gap-2">
-              <span>Custom Text</span>
-              <FaPlayCircle size={22} />
-            </Button>
-          </CardContent>
-        </Card>
-        <Card className=" py-2 px-4 space-y-2">
-          <CardHeader className="p-0 flex flex-col items-center">
-            <CardTitle>Elias</CardTitle>
-            <CardDescription className="">
-              <div>model</div>
-            </CardDescription>
-          </CardHeader>
-          <div className="text-xs flex-1 text-muted-foreground flex justify-between px-2">
-            <div className="flex flex-col items-center justify-center">
-              <span className="font-semibold">sila</span>
-              <span>doaiwdoaw</span>
-            </div>
-            <div className="flex flex-col items-center">
-              <span className="font-semibold">sila</span>
-              <span>doaiwdoaw</span>
-            </div>
-            <div className="flex flex-col items-center">
-              <span className="font-semibold">sila</span>
-              <span>doaiwdoaw</span>
-            </div>
-            <div className="flex flex-col items-center">
-              <span className="font-semibold">sila</span>
-              <span>doaiwdoaw</span>
-            </div>
-          </div>
-
-          <CardContent className="py-2 px-0 space-y-2">
-            <Button variant={"outline"} className="w-full gap-2">
-              <span>Preview</span>
-              <FaPlayCircle size={22} />
-            </Button>
-            <Button className="w-full gap-2">
-              <span>Custom Text</span>
-              <FaPlayCircle size={22} />
-            </Button>
-          </CardContent>
-        </Card>
-        <Card className=" py-2 px-4 space-y-2">
-          <CardHeader className="p-0 flex flex-col items-center">
-            <CardTitle>Elias</CardTitle>
-            <CardDescription className="">
-              <div>model</div>
-            </CardDescription>
-          </CardHeader>
-          <div className="text-xs flex-1 text-muted-foreground flex justify-between px-2">
-            <div className="flex flex-col items-center justify-center">
-              <span className="font-semibold">sila</span>
-              <span>doaiwdoaw</span>
-            </div>
-            <div className="flex flex-col items-center">
-              <span className="font-semibold">sila</span>
-              <span>doaiwdoaw</span>
-            </div>
-            <div className="flex flex-col items-center">
-              <span className="font-semibold">sila</span>
-              <span>doaiwdoaw</span>
-            </div>
-            <div className="flex flex-col items-center">
-              <span className="font-semibold">sila</span>
-              <span>doaiwdoaw</span>
-            </div>
-          </div>
-
-          <CardContent className="py-2 px-0 space-y-2">
-            <Button variant={"outline"} className="w-full gap-2">
-              <span>Preview</span>
-              <FaPlayCircle size={22} />
-            </Button>
-            <Button className="w-full gap-2">
-              <span>Custom Text</span>
-              <FaPlayCircle size={22} />
-            </Button>
-          </CardContent>
-        </Card>
+            <CardContent className="py-2 px-0 space-y-2">
+              <Button variant={"outline"} className="w-full gap-2">
+                <span>Preview</span>
+                <FaPlayCircle size={22} />
+              </Button>
+              <Button className="w-full gap-2">
+                <span>Custom Text</span>
+                <FaPlayCircle size={22} />
+              </Button>
+            </CardContent>
+          </Card>
+        ))}
       </div>
     </div>
   );
